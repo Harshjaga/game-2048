90605dfad33488a9d826ffb29646e44c86881410
Harshjaga <140194847+Harshjaga@users.noreply.github.com>
2023-07-24 23:36:49 +0530

    Add files via upload
    2048 Game
    An easy 2048 Game on website where you can have fun!!
    
    Instructions of the game
    Follow the steps on root repository to clone this whole project.
    Click on the folder 2048 Game.
    Click on the index.html inside the folder.
    Use arrow keys to play.
    Have fun!
    Stack used
    HTML
    Javascript
    CSS
    Credits
    I coded this game by following the tutorial from Code with Ania Kubów. Check it out!
    
    Screenshot

---
+++ for.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    
+    
+    int i=0 , j=0; 
+    int num;
+    for (; i < num, j<num; i++,j++)
+    {   
+    
+        printf("%d %d\n",i,j);
+        
+    }
+    
+}
